592b37a80772fb0b03cfe43deb2e3329602f4693
Gabriele de Capoa <gabriele.decapoa@gmail.com>
2018-10-05 13:58:43 +0200

    Change deploy script (#3)

--- scripts/containers_functions.sh
+++ scripts/containers_functions.sh
@@ -58,10 +58,41 @@ function install_kubectl {
         curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl
         chmod +x ./kubectl
         sudo mv ./kubectl /usr/local/bin/kubectl
-        echo "${GREEN_COLOR}kubectl installed${NO_COLOR}"
+        echo -e "${GREEN_COLOR}kubectl installed${NO_COLOR}"
     else
-        echo "${GREEN_COLOR}kubectl already installed${NO_COLOR}"
+        echo -e "${GREEN_COLOR}kubectl already installed${NO_COLOR}"
     fi
+}
+
+function install_helm {
+    echo -e "${BLUE_COLOR}Check if helm is already installed...${NO_COLOR}"
+    if ! helm help > /dev/null 2>&1; then
+        echo -e "${YELLOW_COLOR}helm is not installed. Installing...${NO_COLOR}"
+        curl https://raw.githubusercontent.com/helm/helm/master/scripts/get > ${HOME}/get_helm.sh
+        chmod 700 ${HOME}/get_helm.sh
+        . ${HOME}/get_helm.sh
+        echo -e "${GREEN_COLOR}helm installed${NO_COLOR}"
+    else
+        echo -e "${GREEN_COLOR}helm already installed${NO_COLOR}"
+    fi
+}
+
+function provision_cluster {
+    echo -e "${BLUE_COLOR}Check if cluster instance exists${NO_COLOR}"
+    if ! bx ks cluster-get ${CLUSTER_NAME} > /dev/null 2>&1; then
+        echo -e "${YELLOW_COLOR}Creating a cluster instance (free plan) named ${CLUSTER_NAME}${NO_COLOR}"
+        bx ks cluster-create --name ${CLUSTER_NAME} --kube-version ${KUBERNETES_VERSION}
+    fi
+    echo -e "${GREEN_COLOR}Cluster ${CLUSTER_NAME} created. You have to wait before provision will be complete${NO_COLOR}"
+    OUTPUT=$(bx cs workers ${CLUSTER_NAME} 2>&1 | grep normal | wc -l)
+    while [[ ${OUTPUT} -eq 0 ]]; do
+        sleep 120
+        echo -e "${YELLOW_COLOR}Checking again after 120 seconds if cluster provisioning is completed${NO_COLOR}"
+    done
+    cmd=$(bx cs cluster-config $CLUSTER_NAME | grep export)
+    $cmd
+    echo -e "${GREEN_COLOR}Cluster ${CLUSTER_NAME} ready and kubectl configured${NO_COLOR}"
+
 }
 
 function build_image {
